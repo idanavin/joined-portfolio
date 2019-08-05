be699b0143b8e11626e1a8a345cee8ba8ac8cc33
Idan <idan.avin@gmail.com>
2019-08-05 18:15:12 +0300

    header and about

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,26 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { HeaderComponent } from './home/header/header.component';
+import { NavbarComponent } from './home/navbar/navbar.component';
+import { AboutComponent } from './home/about/about.component';
+import { ServicesComponent } from './home/services/services.component';
+import { PortfolioComponent } from './home/portfolio/portfolio.component';
+import { ContactComponent } from './home/contact/contact.component';
+import { FooterComponent } from './home/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    HeaderComponent,
+    NavbarComponent,
+    AboutComponent,
+    ServicesComponent,
+    PortfolioComponent,
+    ContactComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
